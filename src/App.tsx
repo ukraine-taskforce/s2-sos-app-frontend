21a52a89745af2c090b3d94109387cb5ca1ecd24
Klim Markelov <tutelaris@list.ru>
2022-03-21 15:52:26 +0100

    Apply review suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './components/layout'
 import AppRoutes from './routes'
-import './App.css';
 
 function App() {
   return (
