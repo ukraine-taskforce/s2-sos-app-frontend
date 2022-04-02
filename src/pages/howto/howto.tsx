88360cc107769c01ee89a1de9892ac7eb13018e9
Olga Ivanova <olga.iva@gmail.com>
2022-04-02 12:25:14 +0200

    wip

--- src/pages/howto/howto.tsx
+++ src/pages/howto/howto.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Button } from '../../others/components/Button';
 import { Header } from '../../others/components/Header';
 import { Spacer } from '../../others/components/Spacer';
 import { Text } from '../../others/components/Text';
 import { Content } from '../../others/components/Content';
+import { Checkmark } from '../../others/components/Checkmark';
 import { ImgNext } from '../../medias/images/UGT_Asset_UI_ButtonNext';
 
 import styles from './howto.module.css';
 export function Howto() {
     const { t } = useTranslation();
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const [displayStep, setDisplayStep] = React.useState(0);
     return (
         <React.Fragment>
@@ -41,13 +42,13 @@ export function Howto() {
                 )}
                 {displayStep === 1 && (
                     <div>
+                        {/* <Checkmark /> */}
                         <Button
                             variant='highlight'
                             fullWidth
                             onClick={() => setDisplayStep(0)}
-                            trailingIcon={<ImgNext alt='' />}
                         >
-                            {t('next')}
+                            {t('start_button_text')}
                         </Button>
                     </div>
                 )}
