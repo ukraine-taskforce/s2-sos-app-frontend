ad9fafccdf70e53172caa55b7d60c8c9bf1edfe6
Thomas Barros Camara <Thomas.Camara@zooplus.com>
2022-04-04 17:11:57 +0200

    Add i18n to phone input component.

--- src/others/components/PhoneInput.tsx
+++ src/others/components/PhoneInput.tsx
@@ -3,6 +3,7 @@ import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/bootstrap.css';
 import styles from "./PhoneInput.module.css";
 import {ImgClose} from "../../medias/images/UGT_Asset_UI_Close";
+import {useTranslation} from "react-i18next";
 
 export interface PhoneInputProps {
     country: string;
@@ -23,6 +24,9 @@ const ClearButton: React.FunctionComponent<ClearButton> = ({onClick}) => {
 }
 
 const PhoneInputCustom: React.FunctionComponent<PhoneInputProps> = ({country, value, placeholder, onChange, isValid = true}) => {
+    const { t } = useTranslation();
+    const countriesListTranslation = JSON.parse(t('landing_phone_input'));
+
     return <div className={`${styles.wrapper} ${!isValid && styles.invalid}`}>
             <PhoneInput country={country}
                                value={value}
@@ -32,6 +36,7 @@ const PhoneInputCustom: React.FunctionComponent<PhoneInputProps> = ({country, va
                                containerClass={styles.container}
                                buttonClass={styles.button}
                                dropdownClass={styles.button}
+                               localization={countriesListTranslation}
                                enableSearch />
 
         {value && value.length > 0 && <ClearButton onClick={() => onChange('', '')} />}
