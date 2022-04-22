9660835c24e9e01942b221a23ed8ad5e0ffd107d
AntonRaichuk <raveman.net@gmail.com>
2022-04-22 14:44:32 +0000

    Change geocoding API to the one that allows referrer restricts

--- src/others/helpers/googleMapsApi.ts
+++ src/others/helpers/googleMapsApi.ts
@@ -1,10 +1,16 @@
-const GOOGLE_API_KEY = 'AIzaSyDa5tT4LZqRjRnbUO5BqHsZNYqG8dIjbqY';
-const MAPS_GOOGLE_API_URL = `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_API_KEY}`;
-
 export const getStreetAddressFromGeolocation = async (lat: number, lng: number) => {
-    const options = 'result_type=street_address&location_type=ROOFTOP';
-    const urlWithLatLngParams = `${MAPS_GOOGLE_API_URL}&latlng=${lat},${lng}&${options}`;
-
-    const response = await fetch(urlWithLatLngParams).then(response => response.json());
-    return response.results.map((r: any) => r.formatted_address).pop();
+  try {
+    const geocoder = new (window as any).google.maps.Geocoder();
+    const response = await geocoder.geocode({ location: {lat: lat, lng: lng} }).then((response: any) => {
+      try {
+        const address = response.results[0].formatted_address;
+        return address;
+      } catch (exc) {
+        return "";
+      }
+    });
+    return response;
+  } catch (exc) {
+    return "";
+  }
 }
