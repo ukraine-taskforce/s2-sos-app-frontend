17fc55d7ed921faf676732e6d53cdb83fe0f50d0
AntonRaichuk <raveman.net@gmail.com>
2022-04-22 15:23:31 +0000

    stuff

--- src/others/helpers/googleMapsApi.ts
+++ src/others/helpers/googleMapsApi.ts
@@ -1,4 +1,7 @@
 export const getStreetAddressFromGeolocation = async (lat: number, lng: number) => {
+  if (!process.env.REACT_APP_GOOGLE_MAPS_API_KEY) {
+    return "api not enabled";
+  }
   try {
     const geocoder = new (window as any).google.maps.Geocoder();
     const response = await geocoder.geocode({ location: {lat: lat, lng: lng} }).then((response: any) => {
